4f31f19478effd730eb228293ed97f8c971e6d2d
nesterov021 <61201519+nesterov021@users.noreply.github.com>
2020-02-25 13:05:29 +0500

    Updates

---
+++ precache-manifest.3b906054fd057bfea31b71eca437454e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a295e03ac7fe077e52b0368d7ea4170",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c072d41320666f432f1",
+    "url": "/static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "674c8e4fda5e92e22d7e",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "9c072d41320666f432f1",
+    "url": "/static/js/2.b31299ba.chunk.js"
+  },
+  {
+    "revision": "674c8e4fda5e92e22d7e",
+    "url": "/static/js/main.9b1fba20.chunk.js"
+  },
+  {
+    "revision": "3310654f64185411c8d9",
+    "url": "/static/js/runtime-main.850ae049.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
