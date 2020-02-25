99d654f6e594f766fb9c02e76b70511a4bc3b490
nesterov021 <61201519+nesterov021@users.noreply.github.com>
2020-02-25 19:17:43 +0500

    Updates

---
+++ precache-manifest.21ea35dd78d4458cada328943cbd2948.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78dea7911ec5132f24096d2e93da89ff",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a960a84b00d32682d885",
+    "url": "./static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "674c8e4fda5e92e22d7e",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "a960a84b00d32682d885",
+    "url": "./static/js/2.fa461049.chunk.js"
+  },
+  {
+    "revision": "674c8e4fda5e92e22d7e",
+    "url": "./static/js/main.9b1fba20.chunk.js"
+  },
+  {
+    "revision": "4f6ff4659378a21e0b5e",
+    "url": "./static/js/runtime-main.3abd18de.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
